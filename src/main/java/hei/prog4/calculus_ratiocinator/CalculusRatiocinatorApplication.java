bad51800b5d30fbfb4bb017c0b745bcb18667fbf
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2024-06-07 13:17:10 +0300

    chore: quickstart mvn archetype with Java 21

---
+++ src/main/java/hei/prog4/calculus_ratiocinator/CalculusRatiocinatorApplication.java
@@ -0,0 +1,13 @@
+package hei.prog4.calculus_ratiocinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculusRatiocinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculusRatiocinatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hei/prog4/calculus_ratiocinator/CalculusRatiocinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.prog4.calculus_ratiocinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculusRatiocinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
