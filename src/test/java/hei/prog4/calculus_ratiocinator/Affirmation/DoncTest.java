052dd6c073598050650f4c6df94d709192a77b05
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2024-06-07 16:07:52 +0300

    feat: "implement all test of Donc conjonction"

---
+++ src/test/java/hei/prog4/calculus_ratiocinator/Affirmation/DoncTest.java
@@ -0,0 +1,40 @@
+package hei.prog4.calculus_ratiocinator.Affirmation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import hei.prog4.calculus_ratiocinator.Logics.Logic;
+import org.junit.jupiter.api.Test;
+
+public class DoncTest {
+    @Test
+    public void test_Lou_est_beau_donc_Lou_est_pauvre() {
+        Affirmation verite = new Verite("Lou est beau");
+        Affirmation mensonge = new Mensonge("Lou est pauvre");
+        Affirmation affirmation = new AffirmationGenerale(verite, mensonge, Logic.DONC);
+        assertFalse(affirmation.estVrai());
+    }
+
+    @Test
+    public void test_Lou_est_pauvre_donc_Lou_est_beau() {
+        Affirmation mensonge = new Mensonge("Lou est pauvre");
+        Affirmation verite = new Verite("Lou est beau");
+        Affirmation affirmation = new AffirmationGenerale(mensonge, verite, Logic.DONC);
+        assertTrue(affirmation.estVrai());
+    }
+
+    @Test
+    public void test_Lou_est_beau_donc_Lou_est_beau() {
+        Affirmation verite1 = new Verite("Lou est beau");
+        Affirmation verite2 = new Verite("Lou est beau");
+        Affirmation affirmation = new AffirmationGenerale(verite1, verite2, Logic.DONC);
+        assertTrue(affirmation.estVrai());
+    }
+
+    @Test
+    public void test_Lou_est_pauvre_donc_Lou_est_pauvre() {
+        Affirmation mensonge1 = new Mensonge("Lou est pauvre");
+        Affirmation mensonge2 = new Mensonge("Lou est pauvre");
+        Affirmation affirmation = new AffirmationGenerale(mensonge1, mensonge2, Logic.DONC);
+        assertTrue(affirmation.estVrai());
+    }
+}
